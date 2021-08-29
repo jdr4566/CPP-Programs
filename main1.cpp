ed176e0f941014c58e949afd8e1d0b5d15ee2ba6
jdr4566 <89748314+jdr4566@users.noreply.github.com>
2021-08-29 14:50:20 -0500

    Add files via upload

---
+++ main1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+  //Declare Variables
+  
+ int num1;
+ int num2;
+ double sum;
+
+ //Ask User for the Numbers
+ cout<<"Please enter your first number: ";
+ cin>>num1;
+ cout<<"Please enter your second number: ";
+ cin>>num2;
+ cout<<"Your first number was: "<<num1<<" and your second number was: "<<num2<<" , finally your total is: "<<sum;
+
+
+
+  return 0;
+
+}

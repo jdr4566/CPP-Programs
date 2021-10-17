efbbcd2651aaaa2f9ae1f56658b7874c9ec27383
JDR4566 <89748314+jdr4566@users.noreply.github.com>
2021-10-16 19:16:42 -0500

    Add files via upload

---
+++ Input Detector.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//Define Variables
+int numberInput;
+char charInput;
+string stringInput;
+
+
+
+int main(){
+
+	cout<< "Please enter a number: "<<endl;
+	cin>>numberInput;
+
+	cout<<"Plase enter a character: "<<endl;
+	cin>>charInput;
+
+	cout<<"Please enter a word: "<<endl;
+	cin>>stringInput;
+
+
+	cout<<"Your number is : "<<numberInput<<endl;
+	cout<<"Your char is : "<<charInput<<endl;
+	cout<<"Your word is : "<<stringInput<<endl;
+
+
+	system("pause");
+}
